e7cdab1cdb28368005fa70f6cc7d9c9570ce01b2
jakemansoor <jakemansoor@oakland.edu>
2024-03-13 19:56:46 -0400

    change2

--- minmax.c
+++ minmax.c
@@ -4,21 +4,36 @@
 
 int main() {
     //values for int data type
-    printf("min and max values of int:\n");
+    printf("\nmin and max values of int:\n");
     printf("Min: %d\n", INT_MIN);
     printf("Max: %d\n", INT_MAX);
+    printf("Overflow min: %d", INT_MIN -1);
+    printf("\nOverflow max: %d", INT_MAX + 1);
+
+
     //values for long data type
-    printf("\nmin and max of long:\n");
-    printf("Min: %d\n", LONG_MIN);
-    printf("Min: %d\n", LONG_MAX);
+    printf("\n\nmin and max of long:\n");
+    printf("Min: %ld\n", LONG_MIN);
+    printf("Max: %ld\n", LONG_MAX);
+    printf("Overflow min: %ld", LONG_MIN - 1);
+    printf("\nOverflow min: %ld", LONG_MAX + 1); 
+
+    
     //values for float data type
-    printf("\nmin and max of float:\n");
-    printf("Min: %d\n", FLT_MIN);
-    printf("Min: %d\n", FLT_MAX);
+    printf("\n\nmin and max of float:\n");
+    printf("Min: %f\n", FLT_MIN);
+    printf("Max: %f\n", FLT_MAX);
+    printf("Overflow min: %f", FLT_MIN - 1);
+    printf("\nOverflow min: %f", FLT_MAX + 1); 
+
+
     //values for double data type
-    printf("\nmin and max of double:\n");
-    printf("Min: %d\n", DBL_MIN);
-    printf("Min: %d\n", DBL_MAX);
+    printf("\n\nmin and max of double:\n");
+    printf("Min: %lf\n", DBL_MIN);
+    printf("Max: %lf\n", DBL_MAX);
+    printf("Overflow min: %lf", DBL_MIN - 1);
+    printf("\nOverflow min: %lf", DBL_MAX + 1); 
+    
 
     return 0;
 }
